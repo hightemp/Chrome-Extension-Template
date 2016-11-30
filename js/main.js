c67ab0c6f9d122198e8cf74a558e8a371ce5a02a
hightemp <hightemp.unknown@gmail.com>
2016-11-30 04:45:33 +0300

    update

--- js/main.js
+++ js/main.js
@@ -3,9 +3,32 @@
 ** description: javascript code for "html/main.html" page
 */
 
-function init_main () {
-    $('html').hide().fadeIn('slow');
+function fnOnDOMContentLoaded () {
+	fnSearchByKeyword("Test");
+}
+
+function fnSearchByKeyword(in_strKeyword) {
+	// Using XMLHttpRequest
+	// xhr.setRequestHeader( 'Api-User-Agent', 'Example/1.0' );
+  alert(in_strKeyword);
+	
+	// Using jQuery
+	$.ajax({
+	    url: "https://www.mediawiki.org/w/api.php",
+	    data: {
+				action: "query",
+				search: "compiler",
+				format: "xml",
+				prop: "info"
+			},
+	    dataType: 'json',
+	    type: 'GET',
+	    headers: { 'Api-User-Agent': 'Example/1.0' },
+	    success: function(data) {
+	       $("#code").text(data);
+	    }
+	});
 }
 
 //bind events to dom elements
-document.addEventListener('DOMContentLoaded', init_main);
+document.addEventListener('DOMContentLoaded', fnOnDOMContentLoaded);
